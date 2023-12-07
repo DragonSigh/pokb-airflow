5fd72576570be96e1941403db7aaa802ae0e764e
DragonSigh <dragonsigh@gmail.com>
2023-12-07 09:27:44 +0300

    another wait change

--- metrics_collector/kornet.py
+++ metrics_collector/kornet.py
@@ -13,7 +13,7 @@ reports_path = config.reports_path
 
 def authorize(login_data: str, password_data: str):
     browser.get("http://llo.emias.mosreg.ru/korvet/admin/signin")
-    browser.implicitly_wait(5)
+    browser.refresh()
     login_field = browser.find_element(
         By.XPATH, '//*[@id="content"]/div/div/form/div[1]/input'
     )
@@ -25,18 +25,23 @@ def authorize(login_data: str, password_data: str):
     browser.find_element(
         By.XPATH, '//*[@id="content"]/div/div/form/div[4]/button'
     ).click()
+
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='ctl00_plate_siteVersion']"))
+    )
+
     logging.info("Авторизация пройдена")
 
 
 def load_dlo_report(begin_date, end_date):
     logging.info("Открываю страницу отчёта")
     browser.get(
-            "http://llo.emias.mosreg.ru/korvet/LocalReportForm.aspx?"
-            "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B&FundingSource=0&BeginDate="
-            + begin_date.strftime("%d.%m.%Y")
-            + "&EndDate="
-            + end_date.strftime("%d.%m.%Y")
-        )
+        "http://llo.emias.mosreg.ru/korvet/LocalReportForm.aspx?"
+        "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B&FundingSource=0&BeginDate="
+        + begin_date.strftime("%d.%m.%Y")
+        + "&EndDate="
+        + end_date.strftime("%d.%m.%Y")
+    )
     browser.implicitly_wait(5)
     logging.info("Отчет сформирован в браузере")
 
@@ -49,16 +54,15 @@ def export_report():
     #    pass
     # Ожидать загрузки отчёта в веб-интерфейсе
     logging.info("Начинается экспорт отчета")
-    #WebDriverWait(browser, 30).until(
-    #    EC.element_to_be_clickable(
-    #        (
-    #            By.XPATH,
-    #            "/html/body/form/table/tbody/tr/td/div/span/div/table/tbody/tr[4]/"
-    #            "td[3]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[8]",
-    #        )
-    #    )
-    #)
-    browser.implicitly_wait(30)
+    WebDriverWait(browser, 30).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "/html/body/form/table/tbody/tr/td/div/span/div/table/tbody/tr[4]/"
+                "td[3]/div/div[1]/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[8]",
+            )
+        )
+    )
     # Выполнить javascript для выгрузки  в Excel, который прописан в кнопке
     browser.execute_script(
         "$find('ctl00_plate_reportViewer').exportReport('EXCELOPENXML');"
