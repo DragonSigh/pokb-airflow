c1f55507fe4c5b8486db7aeddb5daacb0b551609
DragonSigh <dragonsigh@gmail.com>
2023-12-18 12:04:56 +0300

    admission dep changes

---
+++ dag_admission_dep.py
@@ -0,0 +1,39 @@
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+
+def start_hospital_export():
+    import get_admission_dep
+    get_admission_dep.start_hospital_export()
+
+
+def start_bi_export():
+    import get_admission_dep
+    get_admission_dep.start_bi_export()
+
+
+def start_analyze():
+    import get_admission_dep
+    get_admission_dep.start_analyze()
+
+
+default_args = {
+    'start_date': datetime(2023, 1, 1),
+    'sla': timedelta(minutes=60)
+}
+
+dag = DAG(
+    dag_id="hospital_admission_dep",
+    description="Анализ отчетов приемного отделения",
+    schedule_interval="0 6 * * *",
+    catchup=False,
+    default_args=default_args
+)
+
+hospital_export_task = PythonOperator(
+    task_id="hospital_export",
+    python_callable=start_hospital_export,
+    provide_context=True,
+    dag=dag,
+)
---
+++ get_admission_dep.py
@@ -0,0 +1,20 @@
+import metrics_collector.config as config
+import metrics_collector.utils as utils
+import metrics_collector.hospital as hospital
+import metrics_collector.bi_emias as bi_emias
+import json
+
+# Настройки
+PATH_TO_HOSPITAL_CREDENTIAL = r"/home/user/auth-hospital.json"
+METRIC_PATH = r"/etc/samba/share/download/Показатель 22"
+
+
+def start_hospital_export():
+    with open(PATH_TO_HOSPITAL_CREDENTIAL) as f:
+        data = json.load(f)
+
+    auth_username = data["username"]
+    auth_password = data["password"]
+
+    hospital.authorize(auth_username, auth_password)
+    hospital.load_admission_dep_report()
