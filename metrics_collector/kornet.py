1ac78f7c133b0dd46e4566e7e0b7f5cd6b051c58
DragonSigh <dragonsigh@gmail.com>
2024-01-11 09:29:47 +0300

    waiting elements hang fix

--- metrics_collector/kornet.py
+++ metrics_collector/kornet.py
@@ -45,6 +45,11 @@ def authorize(login_data: str, password_data: str):
 def load_dlo_report(begin_date, end_date):
     logging.info("Открываю страницу отчёта")
 
+    browser.get(
+        "http://llo.emias.mosreg.ru/korvet/FiltersLocalReport.aspx?"
+        + "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B"
+    )
+
     browser.find_element(
         By.XPATH, "/html/body/form/header/nav/ul/li[3]/ul/li[2]/a"
     ).click()
@@ -54,14 +59,15 @@ def load_dlo_report(begin_date, end_date):
             (By.XPATH, "//*[@id='aspnetForm']/header/nav/ul/li[3]")
         )
     )
-    
+
     browser.get(
         "http://llo.emias.mosreg.ru/korvet/LocalReportForm.aspx?"
-        "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B&FundingSource=0&BeginDate="
+        + "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B&FundingSource=0&BeginDate="
         + begin_date.strftime("%d.%m.%Y")
         + "&EndDate="
         + end_date.strftime("%d.%m.%Y")
     )
+
     logging.info("Отчет сформирован в браузере")
 
 
