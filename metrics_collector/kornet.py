abf3a1a4e14403da477a171d9222e44c206cc304
DragonSigh <dragonsigh@gmail.com>
2024-01-11 09:01:53 +0300

    modified logic to open report in kornet

--- metrics_collector/kornet.py
+++ metrics_collector/kornet.py
@@ -34,7 +34,9 @@ def authorize(login_data: str, password_data: str):
     ).click()
 
     WebDriverWait(browser, 60).until(
-        EC.presence_of_element_located((By.XPATH, "//*[@id='aspnetForm']/header/nav/ul/li[3]"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[@id='aspnetForm']/header/nav/ul/li[3]")
+        )
     )
 
     logging.info("Авторизация пройдена")
@@ -42,17 +44,29 @@ def authorize(login_data: str, password_data: str):
 
 def load_dlo_report(begin_date, end_date):
     logging.info("Открываю страницу отчёта")
-    browser.get("http://llo.emias.mosreg.ru/korvet/FiltersLocalReport.aspx?"
-                "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B")
-    browser.refresh()
+
     WebDriverWait(browser, 30).until(
         EC.element_to_be_clickable(
             (
                 By.XPATH,
-                "//*[@id='aspnetForm']/header/div/a",
+                "/html/body/form/header/nav/ul/li[3]/a",
             )
         )
     )
+
+    browser.find_element(
+        By.XPATH, "/html/body/form/header/nav/ul/li[3]/ul/li[2]/a"
+    ).click()
+
+    WebDriverWait(browser, 30).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                '//*[@id="ctl00_plate_sumbit"]',
+            )
+        )
+    )
+
     browser.get(
         "http://llo.emias.mosreg.ru/korvet/LocalReportForm.aspx?"
         "guid=85122D62-3F72-40B5-A7ED-B2AFBF27560B&FundingSource=0&BeginDate="
