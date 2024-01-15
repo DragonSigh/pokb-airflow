aae83606a91b6e3d592271c55d25786daeb12834
DragonSigh <dragonsigh@gmail.com>
2024-01-15 15:47:16 +0300

    operator fixed

--- get_admission_dep.py
+++ get_admission_dep.py
@@ -299,7 +299,7 @@ def start_analyze():
                 df_temp,
                 os.path.join(EXPORT_PATH, f"{i[:22]}.xlsx"),
             )
-            df_temp["ФИО Врача"] - df_temp["ФИО Врача"].astype(str)
+            df_temp["ФИО Врача"] = df_temp["ФИО Врача"].astype(str)
             df_temp["ФИО Врача"] = df_temp["ФИО Врача"].apply(
                 lambda x: x.replace(" ", "\n")
             )
