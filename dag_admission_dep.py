c908cbb6f42391a35868395269bb63f33c8ac9b3
DragonSigh <dragonsigh@gmail.com>
2024-01-18 14:23:39 +0300

    added telegram func to send message to channel

--- dag_admission_dep.py
+++ dag_admission_dep.py
@@ -5,28 +5,37 @@ from datetime import datetime, timedelta
 
 def start_hospital_export_run():
     from get_admission_dep import start_hospital_export
+
     start_hospital_export()
 
 
 def start_bi_export_run():
     from get_admission_dep import start_bi_export
+
     start_bi_export()
 
 
 def start_analyze_run():
     from get_admission_dep import start_analyze
+
     start_analyze()
 
 
 def shut_down_run():
     from get_admission_dep import shut_down
+
     shut_down()
 
 
-default_args = {
-    'start_date': datetime(2023, 1, 1),
-    'sla': timedelta(minutes=60)
-}
+def send_message_run():
+    import metrics_collector.telegram as telegram
+
+    text = r"Отчёт по дашборду приёмных отделений успешно сформирован"
+    link = r"`\\10.2.14.224\share\download\Приемные отделения`"
+    telegram.send_telegram_message(telegram.ANALYTICS_CHAT_ID, f"{text}: {link}")
+
+
+default_args = {"start_date": datetime(2023, 1, 1), "sla": timedelta(minutes=60)}
 
 # At hour 9 on every day-of-week from Monday through Friday
 dag = DAG(
@@ -34,7 +43,7 @@ dag = DAG(
     description="Анализ отчетов приемного отделения",
     schedule_interval="40 9 * * 1-5",
     catchup=False,
-    default_args=default_args
+    default_args=default_args,
 )
 
 hospital_export_task = PythonOperator(
@@ -65,6 +74,13 @@ shut_down = PythonOperator(
     dag=dag,
 )
 
+send_message = PythonOperator(
+    task_id="send_message",
+    python_callable=send_message_run,
+    provide_context=True,
+    dag=dag,
+)
+
 bi_export_task >> analyze_task
 hospital_export_task >> analyze_task
-analyze_task >> shut_down
+analyze_task >> shut_down >> send_message
---
+++ metrics_collector/telegram.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from urllib.parse import urlencode
+
+# Чтение данных для авторизации
+PATH_TO_CREDENTIAL = r"/home/user/auth-telegram.json"
+
+with open(PATH_TO_CREDENTIAL) as f:
+    data = json.load(f)
+
+TOKEN = data["token"]
+
+ANALYTICS_CHAT_ID = -1002115902949
+
+
+def send_telegram_message(chat_id: int, message: str):
+    params = {"chat_id": chat_id, "text": message}
+    base_url = f"https://api.telegram.org/bot{TOKEN}/"
+    url = base_url + "sendMessage?" + urlencode(params)
+    resp = requests.get(url)
+    return resp
