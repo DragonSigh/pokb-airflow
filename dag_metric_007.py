3dfdebec3b95a76d733c9c910bd4254ed12b129d
DragonSigh <dragonsigh@gmail.com>
2024-01-18 14:33:18 +0300

    send message to tg in report 7

--- dag_metric_007.py
+++ dag_metric_007.py
@@ -8,6 +8,14 @@ def check_metric_007():
     get_metric_007.check_metric_007()
 
 
+def send_message_run():
+    import metrics_collector.telegram as telegram
+
+    text = r"Отчёт по Показателю 7 успешно сформирован"
+    link = r"`\\10.2.14.224\share\download\Показатель 7`"
+    telegram.send_telegram_message(telegram.ANALYTICS_CHAT_ID, f"{text}: {link}")
+
+
 default_args = {
     'start_date': datetime(2023, 1, 1),
     'sla': timedelta(minutes=60)
@@ -22,8 +30,17 @@ dag = DAG(
     default_args=default_args
 )
 
-python_task = PythonOperator(
+check_metric_task = PythonOperator(
     task_id="run_metric_007",
     python_callable=check_metric_007,
     dag=dag,
 )
+
+send_message = PythonOperator(
+    task_id="send_message",
+    python_callable=send_message_run,
+    provide_context=True,
+    dag=dag,
+)
+
+check_metric_task >> send_message
